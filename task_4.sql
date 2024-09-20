1fa8262b626a169ba076944d97a7fe00943e3869
Amanuel <amanuel.ayalew@aait.edu.et>
2024-09-20 14:54:10 +0300

    fix

--- task_4.sql
+++ task_4.sql
@@ -1,4 +1,4 @@
--- Print the full description of the 'books' table from the 'alx_book_store' database
+-- Print the full description of the 'Books' table from the 'alx_book_store' database
 SELECT 
     COLUMN_NAME, 
     COLUMN_TYPE, 
@@ -10,4 +10,4 @@ FROM
     INFORMATION_SCHEMA.COLUMNS
 WHERE 
     TABLE_SCHEMA = 'alx_book_store' 
-    AND TABLE_NAME = 'books';
+    AND TABLE_NAME = 'Books';
