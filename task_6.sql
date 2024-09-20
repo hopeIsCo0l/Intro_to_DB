cf278657558994660653ad21732267fe5b593744
Amanuel <amanuel.ayalew@aait.edu.et>
2024-09-20 15:02:47 +0300

    task6

--- task_6.sql
+++ task_6.sql
@@ -1,6 +1,11 @@
--- Insert multiple rows into the customer table in the alx_book_store database
+-- Insert row 1 into the customer table
 INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES 
-    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+VALUES (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.');
+
+-- Insert row 2 into the customer table
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.');
+
+-- Insert row 3 into the customer table
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
